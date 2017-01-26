ed1a1937086efeca7bd09a70991eca68dd3f11df
LucileDT <crystale.darck@gmail.com>
2017-01-26 04:29:38 +0100

    Factorisation des headers communs à toutes les pages

--- pages/autres-ressources.php
+++ pages/autres-ressources.php
@@ -2,15 +2,8 @@
 <html>
     <head>
         <title>Ressources insoumises - Portail insoumis</title>
-        <link rel="stylesheet" type="text/css" media="screen" title="Default" href="/styles/v01b00.css" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/elements/favicon-apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="/elements/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/elements/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="/includes/manifest.json">
-        <link rel="mask-icon" href="/elements/favicon-safari-pinned-tab.svg" color="#0198b5">
-        <meta name="apple-mobile-web-app-title" content="Portail insoumis">
-        <meta name="application-name" content="Portail insoumis">
-        <meta name="theme-color" content="#ffffff">
+        <!-- Inclusion des headers communs -->
+        <?php require_once 'includes/common-headers.php'; ?>
     </head>
     <body>
         <div class="column">
