474476ca024754fc7fb17056f289c55a7b4abe7a
TopperFr <christophe.geoffroy@outlook.com>
2017-02-03 15:42:30 +0100

    Suppression du JS

--- pages/includes/common-headers.php
+++ pages/includes/common-headers.php
@@ -1,20 +1,10 @@
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
-
-<link rel="stylesheet" type="text/css" media="screen" title="Default" href="/styles/v01b00.css" />
-
-<link rel="apple-touch-icon" sizes="180x180" href="/elements/favicon-apple-touch-icon.png">
-<link rel="icon" type="image/png" href="/elements/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/elements/favicon-16x16.png" sizes="16x16">
-<link rel="mask-icon" href="/elements/favicon-safari-pinned-tab.svg" color="#0198b5">
-
+<link rel="stylesheet" type="text/css" media="screen" title="Default" href="<?php echo $root; ?>styles/v01b00.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $root; ?>elements/favicon-apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo $root; ?>elements/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo $root; ?>elements/favicon-16x16.png" sizes="16x16">
+<link rel="mask-icon" href="<?php echo $root; ?>elements/favicon-safari-pinned-tab.svg" color="#0198b5">
 <meta name="theme-color" content="#ffffff">
-
 <meta name="apple-mobile-web-app-title" content="Portail insoumis">
 <meta name="application-name" content="Portail insoumis">
-
-<link rel="manifest" href="/includes/manifest.json">
-
-<script     src="https://code.jquery.com/jquery-3.1.1.min.js"
-            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-            crossorigin="anonymous"></script>
-<script src="/javascript/utilitaires.js"></script>
+<meta charset="utf-8" />
+<link rel="manifest" href="<?php echo $root; ?>includes/manifest.json">
